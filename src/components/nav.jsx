7d9568ae54d8e033e89289e1feb30576c1c40687
unknown <you@example.com>
2024-05-13 16:02:30 +0200

    added files

---
+++ src/components/nav.jsx
@@ -0,0 +1,25 @@
+// rafce
+import { useState } from "react";
+ const NavigationBar = () => {
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+    
+  return (
+    <nav className="bg-gray-800">
+        <ul className="flex p-5 text-white text-justify justify-between">
+            <li>navitems</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavigationBar;
+
+// export const notnav = () => {
+//   return (
+//     <div>notnav</div>
+//   )
+// }
